357f6bc885cdf4334d100c413d4197066777179c
James Tauber <jtauber@jtauber.com>
2013-01-30 18:03:19 -0500

    added license and cleaned up a little bit of whitespace

--- church.py
+++ church.py
@@ -6,14 +6,12 @@ FALSE = lambda a: lambda b: (b)
 (FALSE)(True)(False) == False
 
 
-AND   = lambda a: lambda b: (a)(b)(a)
-OR    = lambda a: lambda b: (a)(a)(b)
-NOT   = lambda a: lambda b: lambda c: (a)(c)(b)
+AND = lambda a: lambda b: (a)(b)(a)
+OR  = lambda a: lambda b: (a)(a)(b)
+NOT = lambda a: lambda b: lambda c: (a)(c)(b)
 
 
 (AND)(TRUE)(FALSE) == (FALSE)
-
-
 (AND)(TRUE)(FALSE)(True)(False) == False
 
 
@@ -21,13 +19,17 @@ CONS = lambda a: lambda b: lambda c: (c)(a)(b)
 CAR  = lambda a: (a)(TRUE)
 CDR  = lambda a: (a)(FALSE)
 
+
 (CAR)((CONS)(1)(2)) == 1
 (CDR)((CONS)(1)(2)) == 2
 
+
 UNCHURCH_BOOLEAN = (CONS)(True)(False)
+
 
 (UNCHURCH_BOOLEAN)((NOT)(TRUE)) == False
 (UNCHURCH_BOOLEAN)((OR)(TRUE)(FALSE)) == True
+
 
 ZERO = FALSE
 SUCC = lambda a: lambda b: lambda c: (b)((a)(b)(c))
@@ -37,14 +39,18 @@ TWO = (SUCC)(ONE)
 THREE = (SUCC)(TWO)
 FOUR = (SUCC)(THREE)
 
+
 def church_number(n):
     return SUCC(church_number(n - 1)) if n else FALSE
+
 
 PLUS = lambda a: lambda b: lambda c: lambda d: (a)(c)((b)(c)(d))
 MULT = lambda a: lambda b: lambda c: (b)((a)(c))
 EXP  = lambda a: lambda b: (b)(a)
 
+
 UNCHURCH_NUMBER = lambda a: (a)(lambda b: b + 1)(0)
+
 
 (UNCHURCH_NUMBER)(ZERO) == 0
 (UNCHURCH_NUMBER)(ONE) == 1
